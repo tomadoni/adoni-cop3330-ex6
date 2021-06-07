43742284ad1dc8547c11acf31732f4db85a2ced9
Tom Adoni <tomad101@gmail.com>
2021-06-07 18:11:35 -0400

    Initial commit

---
+++ src/exercise6.java
@@ -0,0 +1,17 @@
+import java.time.Year;
+import java.util.Scanner;
+class Main {
+
+    public static void main(String[] args) {
+	    Scanner kb= new Scanner(System.in);
+	    int x=0;
+	    int y=0;
+		int year = Year.now().getValue();
+	    System.out.println("What is your current age?");
+	    x=kb.nextInt();
+	    System.out.println("At what age would you like to retire?");
+	    y=kb.nextInt();
+	    System.out.println("You have "+(y-x)+" years left until you can retire.");
+	    System.out.println("It's "+year+", so you can retire in "+(year+(y-x)));
+    }
+}
